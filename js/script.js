b5fb019f3466b6f1841438028d02a7b6fb845e8a
jaylenberrien <jaylenberrien.jb@gmail.com>
2023-11-14 13:40:47 -0500

    made an enter button and made find word function

--- js/script.js
+++ js/script.js
@@ -1,8 +1,31 @@
-// let words = require('an-array-of-english-words')
+let words = require('an-array-of-english-words')
 
-// console.log(words[23423].length)
 
 
 function startGame(){
     
 }
+
+//how we get the word for the game
+let fiveWords = []
+
+for(let i = 0; i <= words.length; i++){
+    if(words[i].length === 5){
+        fiveWords.push(words[i])
+    }
+    else{
+        i++
+    }
+}
+
+
+function getRandomindex(min, max){
+    return Math.random() * (6458 - 0) + 0;
+}
+
+
+gameWord = fiveWords[(Math.round(getRandomindex()))]
+
+
+console.log(gameWord)
+
