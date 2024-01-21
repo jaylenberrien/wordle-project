f77b12d5fd9c13ba0fa157b91834f05fb491be6a
jaylenberrien <jaylenberrien.jb@gmail.com>
2024-01-21 18:48:58 -0500

    fixed bug and making a test

--- js/script.js
+++ js/script.js
@@ -629,7 +629,7 @@ function checkLetters(evt){
         hintCounter++
     }
 
-    userGuess[userGuessTracker] = [];
+    // userGuess[userGuessTracker] = [];
     userGuessTracker++
     userLetterTracker = 0;
     attemptTracker--
